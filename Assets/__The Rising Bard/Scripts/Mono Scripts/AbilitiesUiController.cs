9a54b13da0f5cc0f00146ad5221c4146d9531d94
M0Nasser <nasseruser12@gmail.com>
2021-04-21 19:45:53 +0200

    adding ui for the ability

---
+++ Assets/__The Rising Bard/Scripts/Mono Scripts/AbilitiesUiController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AbilitiesUiController: MonoBehaviour
+{
+    [SerializeField] GameObject AbilityMenuUi;
+    [SerializeField] PlayerData playerData;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AbilityMenuUi.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HandelAbilitiesUiCall(InputAction.CallbackContext context)
+    {
+        if (context.performed )
+        {
+            AbilityMenuUi.SetActive(true);
+        }
+        else if(context.canceled)
+        {
+            AbilityMenuUi.SetActive(false);
+        }
+    }
+
+    public void  HandelButtonClick(int value)
+    {
+        playerData.abilities[value].abilityActive = true;
+    }
+
+}
