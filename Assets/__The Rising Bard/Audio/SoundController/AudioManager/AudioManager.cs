ca05e237c921bbd09eedd9678ae966fad074ea77
LAPTOP-6M2E18HV\InternetService <mohmmeuud@gmail.com>
2021-05-09 07:45:46 +0200

    Clips Array - ReadClip Name automatic

--- Assets/__The Rising Bard/Audio/SoundController/AudioManager/AudioManager.cs
+++ Assets/__The Rising Bard/Audio/SoundController/AudioManager/AudioManager.cs
@@ -1,68 +1,97 @@
 using UnityEngine.Audio;
 using System;
 using UnityEngine;
+using UnityEditor;
+
+
 
 public class AudioManager : MonoBehaviour
 {
 
-	public static AudioManager instance;
 
-	//public AudioMixerGroup mixerGroup;
 
-	public Sound[] sounds;
+
+
+    public static AudioManager instance;
+
+
+
+    public Sound[] sounds;
+
+
+
+
+    //public AudioMixerGroup mixerGroup;
+
+    public AudioClip[] clips;
 
 
 
     private void OnEnable()
     {
-		ItemPickSoundHandler.PlaySoundEvent += Play;
+        ItemPickSoundHandler.PlaySoundEvent += PlayMixed;
 
 
-	}
+    }
     private void OnDisable()
     {
-		ItemPickSoundHandler.PlaySoundEvent -= Play;
-	}
+        ItemPickSoundHandler.PlaySoundEvent -= PlayMixed;
+    }
     void Awake()
-	{
-		if (instance != null)
-		{
-			Destroy(gameObject);
-		}
-		else
-		{
-			instance = this;
-			DontDestroyOnLoad(gameObject);
-		}
+    {
 
-		foreach (Sound s in sounds)
-		{
-			s.source = gameObject.AddComponent<AudioSource>();
-			s.source.clip = s.clip;
-			s.source.loop = s.loop;
-			s.source.outputAudioMixerGroup = s.mixerGroup;
-			
-		}
-	}
 
-	public void Play(string sound)
-	{
-		Sound s = Array.Find(sounds, item => item.name == sound);
-		 
-		if (s == null)
-		{
-			Debug.Log("Sound: " + sound + " not found! Please check "+name+ "Object in Scene");
-			return;
-		}
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        int index = 0;
 
-		//s.source.volume = s.volume * (1f + UnityEngine.Random.Range(-s.volumeVariance / 2f, s.volumeVariance / 2f));
-		//s.source.pitch = s.pitch * (1f + UnityEngine.Random.Range(-s.pitchVariance / 2f, s.pitchVariance / 2f));
-		//
-		if (s.source.isPlaying == false)
+
+        foreach (Sound s in sounds)
+        {
+            if (index < clips.Length)
+            {
+
+                s.source = gameObject.AddComponent<AudioSource>();
+                s.clip = clips[index];
+                s.name = clips[index].name;
+
+                s.source.clip = s.clip;
+                Debug.Log(clips[index].name);
+                s.source.loop = s.loop;
+                s.source.outputAudioMixerGroup = s.mixerGroup;
+                index++;
+            }
+
+        }
+    }
+
+    public void Play(string sound)
+    {
+
+        Sound s = Array.Find(sounds, item => item.name == sound);
+
+        if (s == null)
+        {
+            Debug.Log("Sound: " + sound + " not found! Please check " + name + "Object in Scene");
+            return;
+        }
+
+
+        //s.source.volume = s.volume * (1f + UnityEngine.Random.Range(-s.volumeVariance / 2f, s.volumeVariance / 2f));
+        //s.source.pitch = s.pitch * (1f + UnityEngine.Random.Range(-s.pitchVariance / 2f, s.pitchVariance / 2f));
+        //
+        if (s.source.isPlaying == false)
         {
             if (sound == "walk")
             {
-				s.source.time = 23.5f;
+                s.source.time = 23.5f;
             }
             s.source.Play();
 
@@ -86,10 +115,35 @@ public class AudioManager : MonoBehaviour
             Debug.Log("Sound: " + sound + " not found! Please check " + name + "Object in Scene");
             return;
         }
- 
-            s.source.Stop();
-        
+
+        s.source.Stop();
+
 
     }
+
+
+
+
+    public void PlayMixed(string sound)
+    {
+
+        Sound s = Array.Find(sounds, item => item.name == sound);
+
+        if (s == null)
+        {
+            Debug.Log("Sound: " + sound + " not found! Please check " + name + "Object in Scene");
+            return;
+        }
+
+
+
+
+ 
+ 
+        s.source.PlayOneShot(s.source.clip);
+
+
+    }
+
 
 }
