d05545d171543f2f4e40fc35d6e5b7d2f06c7a53
duhaemad <75424796+duhaemad@users.noreply.github.com>
2021-05-16 13:04:57 +0200

    mainmenu

---
+++ Assets/__The Rising Bard/Scripts/UI/UIManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class UIManager : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject mainmenuPanel;
+    [SerializeField]
+    GameObject settingsPanel;
+    private void Awake()
+    {
+        mainmenuPanel.SetActive(true);
+        settingsPanel.SetActive(false);
+    }
+    public void Settings()
+    {
+        mainmenuPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("UITRY");
+
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
