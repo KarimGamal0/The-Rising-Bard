506d4d9380fc488b6d2ff92776dcf34a831621e1
LAPTOP-6M2E18HV\InternetService <mohmmeuud@gmail.com>
2021-05-22 03:19:36 +0200

    Fixing falling obstacle (add constrains - player will not die after obstacle fall - calling death event ) Add Level Manager (reload scene - restart function - with courtine ) changed playercombact script death function call ( instead of update called from damge ) add flower prefab - particle system - disable all children  -enable on pick . fixed spikes groups collider and position removed deathscript (its not good to call death from many palces ) add death event to obstacle to show particles event .

--- Assets/__The Rising Bard/Scripts/Game Data Handlers/levelManagerCareTaker.cs
+++ Assets/__The Rising Bard/Scripts/Game Data Handlers/levelManagerCareTaker.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class levelManagerCareTaker : MonoBehaviour
 {
+
+
+    [SerializeField] PlayerData PD;
     private Dictionary<string, Memento> mementoDic = new Dictionary<string, Memento>();
     public static levelManagerCareTaker instance;
 
@@ -18,34 +21,70 @@ public class levelManagerCareTaker : MonoBehaviour
             instance = this;
             //DontDestroyOnLoad(gameObject);
         }
+
+
+
     }
     public void add(Memento checkPointSnap, string key)
     {
- 
+
 
         mementoDic.Add(key, checkPointSnap);
-     
+
         Debug.Log($"last add key{key}");
 
     }
 
-    
+
     public Memento get(string key)//get momento with key (name) ..you have to use add with string ... 
     {
         if (mementoDic.ContainsKey(key))
         {
             Memento val;
             mementoDic.TryGetValue(key, out val);
-            return val ;
+            return val;
         }
         else
         {
             Debug.LogError("Ensure u have check points in yout scene ");
-            return new Memento(0,0,new Vector2(1,1));
+            return null;
         }
     }
+
+
+    IEnumerator OnRestart()
+    {
+
+
+        yield return new WaitForSeconds(1);//null;
+        PD.playerHP = 100;
+        PD.playerMana = 100;
+        PD.playerScore = 100;
+        // Destroy(playerobj);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+    }
+
+    void RestartGame()
+    {
+        StartCoroutine(OnRestart());
+    }
+
+    private void OnEnable()
+    {
+        PlayerCombatController.playerDeathE += RestartGame;
+    }
+
+    private void OnDisable()
+    {
+        PlayerCombatController.playerDeathE -= RestartGame;
+    }
+
+
+
+
 }
-    
+
 
 //public Memento get(int index)
 //{
