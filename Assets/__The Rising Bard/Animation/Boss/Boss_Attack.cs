fc80c07afa2272992e7988e07864c34c9b2201f2
Karim-PC <kgamalali@gmail.com>
2021-05-25 15:58:44 +0200

    enemy fix bugs and added tested boss

---
+++ Assets/__The Rising Bard/Animation/Boss/Boss_Attack.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_Attack : StateMachineBehaviour
+{
+    [SerializeField]
+    float speed = 2.5f;
+    [SerializeField]
+    float attackRange = 3;
+
+    Transform player;
+    Rigidbody2D rb2d;
+    Boss boss;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb2d = animator.GetComponent<Rigidbody2D>();
+        boss = animator.GetComponent<Boss>();
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        boss.LookAtPlayer();
+
+        Vector2 target = new Vector2(player.position.x, rb2d.position.y);
+
+        if (Vector2.Distance(player.position, rb2d.position) <= attackRange)
+        {
+            animator.SetBool("isAttacking",true);
+        }
+        else
+        {
+            animator.SetBool("isAttacking", false);
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
