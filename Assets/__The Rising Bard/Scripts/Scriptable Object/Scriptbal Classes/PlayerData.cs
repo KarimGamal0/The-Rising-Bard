ea69e85884ab51e827bc5068b43abf8a1ad4077f
M0Nasser <nasseruser12@gmail.com>
2021-06-03 17:02:01 +0200

    adding each level loader adding each level data adding death loctions

--- Assets/__The Rising Bard/Scripts/Scriptable Object/Scriptbal Classes/PlayerData.cs
+++ Assets/__The Rising Bard/Scripts/Scriptable Object/Scriptbal Classes/PlayerData.cs
@@ -20,8 +20,41 @@ public class PlayerData : ScriptableObject
     public float playerScore = 0;
     public void restoreOrignalData()
     {
-         playerHP = 100;
-         playerMana = 100;
+        playerHP = 100;
+        playerMana = 100;
+    }
+
+    public void startFirstLevelData()
+    {
+        playerHP = 100;
+        playerMana = 100;
+        foreach (var item in abilities)
+        {
+            item.abilityGained = false;
+        }
+    }
+
+    public void startSecondLevelData()
+    {
+        playerHP = 100;
+        playerMana = 100;
+        foreach (var item in abilities)
+        {
+            item.abilityGained = false;
+        }
+    }
+
+    public void startThirdLevelData()
+    {
+        playerHP = 100;
+        playerMana = 100;
+        for (int i = 0; i < 6; i++)
+        {
+            if (i > 2)
+                abilities[i].abilityGained = true;
+            else
+                abilities[i].abilityGained = false;
+        }
     }
 
 }
