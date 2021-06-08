cf48719ab94adf006f7c35a044d93520bca8a6f7
M0Nasser <nasseruser12@gmail.com>
2021-06-09 01:30:54 +0200

    adding some elements to the presention

--- Assets/__The Rising Bard/Scripts/Mono Scripts/items/CameraSwitchWithOutEvent.cs
+++ Assets/__The Rising Bard/Scripts/Mono Scripts/items/CameraSwitchWithOutEvent.cs
@@ -7,27 +7,45 @@ public class CameraSwitchWithOutEvent : MonoBehaviour
 {
     [SerializeField] CinemachineVirtualCamera firstCM;
     [SerializeField] CinemachineVirtualCamera secondCM;
+    [SerializeField] GameObject canvasGameObject;
+
+
+    private void Awake()
+    {
+        canvasGameObject.SetActive(false);
+    }
 
 
     public void SwitchToFirstCam()
     {
         firstCM.Priority = 10;
         secondCM.Priority = 2;
+        StartCoroutine(DelayAction(2));
+        
     }
     public void SwitchToSecondCam()
     {
         firstCM.Priority = 2;
         secondCM.Priority = 10;
+        canvasGameObject.SetActive(true);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
+        {
             SwitchToSecondCam();
+        }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.tag == "Player")
             SwitchToFirstCam();
+    }
+
+    IEnumerator DelayAction(float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        canvasGameObject.SetActive(false);
     }
 }
