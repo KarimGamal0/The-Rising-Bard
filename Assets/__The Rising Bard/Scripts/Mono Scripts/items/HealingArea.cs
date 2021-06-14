6156f6de6a70e18f68e0981c70f9ed0c2ffe3843
M0Nasser <nasseruser12@gmail.com>
2021-06-15 01:30:33 +0200

    edit healing area

--- Assets/__The Rising Bard/Scripts/Mono Scripts/items/HealingArea.cs
+++ Assets/__The Rising Bard/Scripts/Mono Scripts/items/HealingArea.cs
@@ -6,12 +6,19 @@ public class HealingArea : MonoBehaviour
 {
     [SerializeField] Material defultMat;
     [SerializeField] Material matToChange;
+    [SerializeField] Color BaseColor;
 
+    private Animator playerAnimator ;
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        //Debug.Log( collision.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("W_Idle"));
+        if (collision.tag == "Player"/*&&collision.GetComponent<PlayerOldControlles>().is*/)
         {
+            playerAnimator = collision.GetComponent<Animator>();
+            if(playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("W_Idle") &&
+            playerAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f)
             collision.GetComponent<Renderer>().material = matToChange;
+            //todo : add heath for the player
         }
     }
 
